4ee2babf4381b4a540a91138a065d475be832242
Akshay Nair <phenax5@gmail.com>
2021-01-08 15:45:14 +0530

    Moves to building with nix

--- default.nix
+++ default.nix
@@ -1,14 +1,33 @@
-{ nixpkgs ? import <nixpkgs> {} }:
+{ nixpkgs ? import <nixpkgs> {}, haskellPackages ? nixpkgs.haskellPackages, compiler ? "default", doBenchmark ? false }:
+
 let
   inherit (nixpkgs) pkgs;
-  inherit (pkgs) haskellPackages;
-
-  nixPackages = [
+  systemPackages = [
     haskellPackages.haskell-language-server
     haskellPackages.cabal-install
     pkgs.entr # Re-run on file change
   ];
-in pkgs.stdenv.mkDerivation {
-  name = "env";
-  buildInputs = nixPackages;
-}
+
+  commonHsPackages = with haskellPackages; [
+    base
+    bytestring
+    containers
+    file-embed
+    parsec
+    raw-strings-qq
+    transformers
+  ];
+in
+  with haskellPackages; mkDerivation {
+    pname = "simlisp";
+    version = "0.1.0.0";
+    src = ./.;
+    isLibrary = true;
+    isExecutable = true;
+    libraryHaskellDepends = commonHsPackages;
+    executableHaskellDepends = commonHsPackages;
+    executableSystemDepends = systemPackages;
+    testHaskellDepends = commonHsPackages ++ [ hspec ];
+    license = "MIT";
+    hydraPlatforms = stdenv.lib.platforms.none;
+  }
